51af43c36884e361a8a7c2c3172de5907a1b00bc
UncertainProd <83609901+UncertainProd@users.noreply.github.com>
2023-06-07 23:03:45 +0530

    Testing gh pages(1)

--- rust-img-lib/Cargo.toml.d.ts
+++
@@ -1,5 +0,0 @@
-export * from "../rust-img-lib/pkg/rust_img_lib";
-
-type Exports = typeof import("../rust-img-lib/pkg/rust_img_lib");
-declare const init: () => Promise<Exports>;
-export default init;
--- svelte-frontend/src/global.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference types="svelte" />
-export type Wasm_T = typeof import("../../rust-img-lib/pkg/rust_img_lib");
